583f3be10c9e3faba0a2ce29796beb6e3cdb1e85
isidro <idperez@asu.edu>
2018-02-22 08:01:47 -0700

    Constants class added

---
+++ Utils/src/main/java/Constants.java
@@ -0,0 +1,12 @@
+/**
+ * Constants
+ *
+ * Constants used by the Client and Server
+ *
+ * @author Team 7
+ * @version 1.0
+ */
+public class Constants {
+    // declare constants here
+    // client and server modules will have access to them
+}
