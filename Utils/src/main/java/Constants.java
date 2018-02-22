66822fd18e9c4bc022e02f037f5a5f53870a1a53
Dileep Pandey <dileep.pandey@asu.edu>
2018-02-22 08:36:11 -0700

    Constant class updated

--- Utils/src/main/java/Constants.java
+++ Utils/src/main/java/Constants.java
@@ -9,4 +9,11 @@
 public class Constants {
     // declare constants here
     // client and server modules will have access to them
+    public static final Color COLOR_BLUE = new Color(153, 153, 255);
+    public static final Color COLOR_PINK = new Color(255, 204, 204);
+    public static final Color COLOR_GRAY = new Color(200, 200, 200);
+    public static final String HIGHEST_VAL_LBL = "Highest Value";
+    public static final String LOWEST_VAL_LBL = "Lowest Value";
+    public static final String FREQUENCY_LBL = "Frequency(Hz)";
+    public static final int TCP_PORT = 8080;
 }
