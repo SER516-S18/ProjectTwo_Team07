1baa63d75522a227166ec4099978c482ff54946b
Gopika Pai <11135326+gopikaspai@users.noreply.github.com>
2018-02-22 00:33:20 -0700

    Update ServerStatus.java

--- Server/src/main/java/ServerStatus.java
+++ Server/src/main/java/ServerStatus.java
@@ -1,12 +1,56 @@
-public class ServerStatus {
+-public class ServerStatus {
 
+import java.io.IOException;
+import java.net.DatagramSocket;
+import java.net.ServerSocket;
+import javax.swing.*;
+
+public class ServerStatus extends JComponent{
+    
+   
     public ServerStatus(){}
 
     public static void startBlinking(){
         // TODO add your handling code here:
+        if(isServerRunning(8080)){             
+            System.out.println("Running");
+        }
     }
+     private void initComponents() {
 
+       
+     }
+    public static boolean isServerRunning(int port) {
+       // int port=8080;
+    ServerSocket ss = null;
+    DatagramSocket ds = null;
+    try {
+        ss = new ServerSocket(port);
+        ss.setReuseAddress(true);
+        ds = new DatagramSocket(port);
+        ds.setReuseAddress(true);
+        return false;
+    } catch (IOException e) {
+        return true;
+    } finally {
+        if (ds != null) {
+            ds.close();
+        }
+
+        if (ss != null) {
+            try {
+                ss.close();
+            } catch (IOException e) {
+                /* should not be thrown */
+            return true;
+            }
+        }
+    }
+    }
     public static void stopBlinking(){
         // TODO add your handling code here:
+    if(!isServerRunning(8080)){
+        System.out.println("Not Running");
+    }
     }
 }
