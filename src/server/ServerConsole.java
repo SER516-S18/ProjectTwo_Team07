808ad2ab02e893544d02cbdbac55d4b5863dd9f2
Abhishek Parmar <35242470+AbhishekParmar81@users.noreply.github.com>
2018-02-23 12:58:43 -0700

    ServerConsole.java

--- src/server/ServerConsole.java
+++ src/server/ServerConsole.java
@@ -1,13 +1,52 @@
 package server;
 
+import java.awt.Color;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JTextPane;
+import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.StyleConstants;
+import javax.swing.text.StyledDocument;
+
 public class ServerConsole {
-    public ServerConsole(){}
 
-    public static void setErrorMessage(String errorMessage) {
-        // TODO add your handling code here:
-    }
+	private final static Logger LOGGER = Logger.getLogger(ServerConsole.class.getName());
+	private JTextPane console = new ServerUI().getConsolePane();
+	
+	public ServerConsole() {
+	
+	}
+	
+	public void setErrorMessage(String errorMessage) {
 
-    public static void setMessage(String message) {
-        // TODO add your handling code here:
-    }
+		StyledDocument doc = (StyledDocument) console.getDocument();
+		SimpleAttributeSet keyWord = new SimpleAttributeSet();
+		StyleConstants.setForeground(keyWord, Color.RED);
+		StyleConstants.setFontFamily(keyWord, "Times New Roman");
+		StyleConstants.setBackground(keyWord, Color.LIGHT_GRAY);
+		try
+		{
+			doc.insertString(0,new Date()+" - Error - "+errorMessage +"\n",keyWord );
+		}
+		catch(Exception ex) 
+		{ 
+			LOGGER.log(Level.SEVERE,"Exception while adding Error Message.", ex); 
+		}
+
+	}
+
+	public void setMessage(String message) {
+		try {
+			StyledDocument doc = (StyledDocument) console.getDocument();
+			SimpleAttributeSet keyWord = new SimpleAttributeSet();
+			StyleConstants.setForeground(keyWord, Color.BLACK);
+			StyleConstants.setFontFamily(keyWord, "Times New Roman");
+			StyleConstants.setBackground(keyWord, Color.LIGHT_GRAY);
+			doc.insertString(0,new Date()+"- Message - "+message+"\n",keyWord );
+		} catch (Exception ex) {
+			LOGGER.log(Level.SEVERE,"Exception while adding Message", ex);
+		}
+	}
 }
+
