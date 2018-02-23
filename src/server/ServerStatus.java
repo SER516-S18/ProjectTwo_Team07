63845a14d410f552b8a65a9e686adb8e25c3d56b
Gopika Pai <11135326+gopikaspai@users.noreply.github.com>
2018-02-22 23:55:14 -0700

    Update ServerStatus.java

--- src/server/ServerStatus.java
+++ src/server/ServerStatus.java
@@ -7,10 +7,12 @@ import javax.swing.*;
 
 public class ServerStatus extends JComponent{
   /**
-    * 
+    * This code displays a message on the server if the server is
+    * running or not. It will be shown in the rectangle box on the 
+    * server next to the Highest, Lowest values. 
     */
     public static JPanel pPanel = ServerUI.getPlotPanel();
-    public static void startBlinking(){
+    public static void startMessage(){
             if(isServerRunning(8080)){  
                 JLabel nj  = new JLabel();
                 nj.setText("Not Running....");
@@ -18,39 +20,45 @@ public class ServerStatus extends JComponent{
                 System.out.println("Running");
             }
         }
-    public static boolean isServerRunning(int port) {
-       // int port=8080;
-    ServerSocket server_socket = null;
-    DatagramSocket datagram_socket = null;
-    try {
-        server_socket = new ServerSocket(port);
-        server_socket.setReuseAddress(true);
-        datagram_socket = new DatagramSocket(port);
-        datagram_socket.setReuseAddress(true);
-        return false;
-    } catch (IOException e) {
-        return true;
-    } finally {
-        if (datagram_socket != null) {
-            datagram_socket.close();
-        }
-
-        if (server_socket != null) {
-            try {
-                server_socket.close();
-            } catch (IOException e) {
-                /* should not be thrown */
-            return true;
-            }
-        }
-    }
-    }
-    public static void stopBlinking(){
+    public static void stopMessage(){
         if(!isServerRunning(8080)){
             JLabel nj  = new JLabel();
             nj.setText("Not Running....");
             pPanel.add(nj);
             System.out.println("Not Running");
-        }
-        }
+         }
+     }
+  
+  /**
+    * The server sockets here checks if the ports
+    * are in use or not.
+    */
+  
+   public static boolean isServerRunning(int port) {
+       // int port=8080;
+        ServerSocket server_socket = null;
+        DatagramSocket datagram_socket = null;
+        try {
+          server_socket = new ServerSocket(port);
+          server_socket.setReuseAddress(true);
+          datagram_socket = new DatagramSocket(port);
+          datagram_socket.setReuseAddress(true);
+          return false;
+      } catch (IOException e) {
+          return true;
+      } finally {
+          if (datagram_socket != null) {
+              datagram_socket.close();
+          }
+
+          if (server_socket != null) {
+              try {
+                  server_socket.close();
+              } catch (IOException e) {
+                /* should not be thrown */
+              return true;
+              }
+          }
+       }
+    }
 }
