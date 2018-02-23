a64e43894076c105c303f9c41987543e4811b12e
isidro <idperez@asu.edu>
2018-02-22 19:27:02 -0700

    response object updated

--- src/network/Response.java
+++ src/network/Response.java
@@ -10,16 +10,36 @@ package network;
  */
 public class Response {
     private int[] channelNumbers;
+    private boolean frequencyChange;
+    private int frequency;
 
-    public Response(int[] channelNumbers) {
+    public Response(int[] channelNumbers, boolean frequencyChange, int frequency) {
         this.channelNumbers = channelNumbers;
+        this.frequencyChange = frequencyChange;
+        this.frequency = frequency;
     }
 
     public int[] getChannelNumbers() {
         return channelNumbers;
     }
 
+    public boolean isFrequencyChange() {
+        return frequencyChange;
+    }
+
+    public int getFrequency() {
+        return frequency;
+    }
+
     public void setChannelNumbers(int[] channelNumbers) {
         this.channelNumbers = channelNumbers;
+    }
+
+    public void setFrequencyChange(boolean frequencyChange) {
+        this.frequencyChange = frequencyChange;
+    }
+
+    public void setFrequency(int frequency) {
+        this.frequency = frequency;
     }
 }
