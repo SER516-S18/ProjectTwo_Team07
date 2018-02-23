749701443a9e1d5dce82c04c948cfc8e945f9a6d
isidro <idperez@asu.edu>
2018-02-22 19:06:52 -0700

    restore client classes

---
+++ src/client/ClientConsole.java
@@ -0,0 +1,48 @@
+package client;
+
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.swing.JTextPane;
+import javax.swing.text.html.HTMLDocument;
+
+/**
+ * A class to handle displaying message and error message in the client console
+ *
+ * @author raamprashanthns
+ * @version 1
+ */
+public class ClientConsole {
+
+    static JTextPane console = ClientUI.getConsoleTextArea();
+    private final static Logger LOGGER = Logger.getLogger(ClientConsole.class.getName());
+
+    /**
+     * Used to display error messages in the client console
+     *
+     * @param errorMessage - error message that has to be displayed in the client console
+     */
+    public static void setErrorMessage(String errorMessage) {
+        try {
+            HTMLDocument doc=(HTMLDocument) console.getStyledDocument();
+            doc.insertAfterEnd(doc.getCharacterElement(doc.getLength()),"<span style=\"color:red\">" + new Date() + " " + errorMessage + "</span> <br>");
+        } catch (Exception ex) {
+            LOGGER.log(Level.SEVERE, "Exception while adding Error Message to client console", ex);
+        }
+    }
+
+    /**
+     * Used to display messages in the client console
+     *
+     * @param message - message that has to be displayed in the client console
+     */
+    public static void setMessage(String message) {
+        try {
+            HTMLDocument doc=(HTMLDocument) console.getStyledDocument();
+            doc.insertAfterEnd(doc.getCharacterElement(doc.getLength()),"<span style=\"color:black\">" + new Date() + " " + message + "</span> <br>");
+        } catch (Exception ex) {
+            LOGGER.log(Level.SEVERE, "Exception while adding Message to client console", ex);
+        }
+    }
+}
