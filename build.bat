f54d995447bd33b42e7d119ea11434db61c3e190
Dileep Pandey <dileep.pandey@asu.edu>
2018-02-23 21:24:54 -0700

    batch file updated

--- build.bat
+++ build.bat
@@ -12,14 +12,14 @@ del /f /Q client.jar
 
 echo Starting build...
 
-javac -d . -cp "%ROOT_PATH%src;%LIB_PATH%\kryo-4.0.1.jar;%LIB_PATH%\kryonet-2.22.0-RC1.jar;%LIB_PATH%\jfreechart-1.0.13.jar;%LIB_PATH%\jcommon-1.0.16.jar;%LIB_PATH%\minlog-1.3.0.jar;%LIB_PATH%\objenesis-2.1.jar;%LIB_PATH%\reflectasm-1.10.1-shaded.jar" %ROOT_PATH%src\server\ServerApp.java
+javac -d . -cp "%ROOT_PATH%src;%LIB_PATH%\kryo-4.0.1.jar;%LIB_PATH%\kryonet-2.22.0-RC1.jar;%LIB_PATH%\jfreechart-1.0.13.jar;%LIB_PATH%\jcommon-1.0.16.jar;%LIB_PATH%\minlog-1.3.0.jar;%LIB_PATH%\objenesis-2.1.jar;%LIB_PATH%\reflectasm-1.10.1-shaded.jar" %ROOT_PATH%src\server\ServerMain.java
 
-jar cfm server.jar /manifest/manifest_server.txt lib src server network util
+jar cfm server.jar %ROOT_PATH%\manifest\server_manifest.txt lib src server network util
 del /f /Q server
 
-javac -d . -cp "%ROOT_PATH%src;%LIB_PATH%\kryo-4.0.1.jar;%LIB_PATH%\kryonet-2.22.0-RC1.jar;%LIB_PATH%\jfreechart-1.0.13.jar;%LIB_PATH%\jcommon-1.0.16.jar;%LIB_PATH%\minlog-1.3.0.jar;%LIB_PATH%\objenesis-2.1.jar;%LIB_PATH%\reflectasm-1.10.1-shaded.jar" %ROOT_PATH%src\client\ClientApp.java
+javac -d . -cp "%ROOT_PATH%src;%LIB_PATH%\kryo-4.0.1.jar;%LIB_PATH%\kryonet-2.22.0-RC1.jar;%LIB_PATH%\jfreechart-1.0.13.jar;%LIB_PATH%\jcommon-1.0.16.jar;%LIB_PATH%\minlog-1.3.0.jar;%LIB_PATH%\objenesis-2.1.jar;%LIB_PATH%\reflectasm-1.10.1-shaded.jar" %ROOT_PATH%src\client\ClientMain.java
 
-jar cfm client.jar /manifest/manifest_client.txt lib src client network util
+jar cfm client.jar %ROOT_PATH%\manifest\client_manifest.txt lib src client network util
 del /f /Q client
 del /f /Q network
 del /f /Q util
