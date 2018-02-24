4837e33d23ead42d84e1cd3ff9617aad296b094f
sumanparlapapalli <32560375+sumanparlapapalli@users.noreply.github.com>
2018-02-23 21:50:59 -0700

    Add files via upload
    Added comments

--- src/server/ServerFrequency.java
+++ src/server/ServerFrequency.java
@@ -1,18 +1,26 @@
 package server;
 import javax.swing.JTextField;
+/**
+ * ServerFrequency
+ *
+ * Reads frequency from the GUI and returns it.
+ * @param frequency
+ * @author Team 7
+ * @version 1.0
+ */
 
 public class ServerFrequency {
-	public static int frequency(JTextField frequencyTxt) {
+	public static int frequency(JTextField frequencyValueText) {
 		int frequency = 0;
-        if(frequencyTxt.getText() != "")
+        if(frequencyValueText.getText() != "")
         {
             try
             {
-                frequency = Integer.parseInt(frequencyTxt.getText());
+                frequency = Integer.parseInt(frequencyValueText.getText());
             }
             catch(Exception e)
             {
-                System.out.println("Default value is returned.");
+                e.printStackTrace();
             }
         }
         return frequency;
